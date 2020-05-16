9ec30fcc9ea1655ab826412b7ed870fe696ff750
NL <NL@NLs-MacBook-Air.fios-router.home>
2020-05-16 18:54:22 -0400

    added index.html .css .js

---
+++ index.js
@@ -0,0 +1,137 @@
+const STORE =[
+{
+    questions: 'Dune was first published by which publisher?',
+    answers: [
+        'A) Chilton Books',
+        'B) Analog Magazine',
+        'C) Tom Doherty Associates',
+        'D) Penguin Random House',
+        'E) Macmillan'
+      ],
+      correctAnswer: 'A) Chilton Books',
+
+},
+{
+    questions: 'What Inspire Frank Herbert to set the desert as a setting of his story',
+    answers: [
+        'A) A trip to Oregon',
+        'B) trip to the Sahara Desert',
+        'C) Tom Doherty Associates',
+        'D) Penguin Random House',
+        'E) Macmillan'
+      ],
+      correctAnswer: 'A) Chilton Books',
+
+},
+{
+    questions: 'Dune was first published by which publisher?',
+    answers: [
+        'A) Chilton Books',
+        'B) Analog Magazine',
+        'C) Tom Doherty Associates',
+        'D) Penguin Random House',
+        'E) Macmillan'
+      ],
+      correctAnswer: 'A) Chilton Books',
+
+},
+{
+    questions: 'Dune was first published by which publisher?',
+    answers: [
+        'A) Chilton Books',
+        'B) Analog Magazine',
+        'C) Tom Doherty Associates',
+        'D) Penguin Random House',
+        'E) Macmillan'
+      ],
+      correctAnswer: 'A) Chilton Books',
+
+},
+{
+    questions: 'Dune was first published by which publisher?',
+    answers: [
+        'A) Chilton Books',
+        'B) Analog Magazine',
+        'C) Tom Doherty Associates',
+        'D) Penguin Random House',
+        'E) Macmillan'
+      ],
+      correctAnswer: 'A) Chilton Books',
+
+},
+{
+    questions: 'Dune was first published by which publisher?',
+    answers: [
+        'A) Chilton Books',
+        'B) Analog Magazine',
+        'C) Tom Doherty Associates',
+        'D) Penguin Random House',
+        'E) Macmillan'
+      ],
+      correctAnswer: 'A) Chilton Books',
+
+},
+{
+    questions: 'Dune was first published by which publisher?',
+    answers: [
+        'A) Chilton Books',
+        'B) Analog Magazine',
+        'C) Tom Doherty Associates',
+        'D) Penguin Random House',
+        'E) Macmillan'
+      ],
+      correctAnswer: 'A) Chilton Books',
+
+},
+{
+    questions: 'Dune was first published by which publisher?',
+    answers: [
+        'A) Chilton Books',
+        'B) Analog Magazine',
+        'C) Tom Doherty Associates',
+        'D) Penguin Random House',
+        'E) Macmillan'
+      ],
+      correctAnswer: 'A) Chilton Books',
+
+},
+{
+    questions: 'Dune was first published by which publisher?',
+    answers: [
+        'A) Chilton Books',
+        'B) Analog Magazine',
+        'C) Tom Doherty Associates',
+        'D) Penguin Random House',
+        'E) Macmillan'
+      ],
+      correctAnswer: 'A) Chilton Books',
+
+},
+{
+    questions: 'Dune was first published by which publisher?',
+    answers: [
+        'A) Chilton Books',
+        'B) Analog Magazine',
+        'C) Tom Doherty Associates',
+        'D) Penguin Random House',
+        'E) Macmillan'
+      ],
+      correctAnswer: 'A) Chilton Books',
+
+},
+
+];
+
+
+
+
+function handleQuizClicks(){
+    sstartQuiz();
+    renderAQuestion();
+    submitAnswer();
+    nextQuestion();
+    restartQuiz();
+    }
+}
+
+$(handleQuizClicks());
