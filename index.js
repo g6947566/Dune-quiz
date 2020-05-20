56d0694c9211264790b1ab2489f9bda086088884
NL <NL@NLs-MacBook-Air.fios-router.home>
2020-05-20 00:18:24 -0400

    show correct answers and HTML seperated

--- index.js
+++ index.js
@@ -1,253 +1,266 @@
-const STORE =[
-{
+const STORE = [{
     question: '1. Dune was first published by which publisher?',
     answers: [
-        'A) Chilton Books',
-        'B) Analog Magazine',
-        'C) Tom Doherty',
-        'D) Penguin',
-        'E) Macmillan'
-      ],
-      correctAnswer: 'A) Chilton Books'
+      'A) Chilton Books',
+      'B) Analog Magazine',
+      'C) Tom Doherty',
+      'D) Penguin',
+      'E) Macmillan'
+    ],
+    correctAnswer: 'A) Chilton Books'
 
-},
-{
+  },
+  {
     question: '2. What Inspire Frank Herbert to set the desert as a setting of his story',
     answers: [
-        'A) A trip to Oregon',
-        'B) A trip to Sahara Desert',
-        'C) A trip to Mexico',
-        'D) A trip to Florida',
-        'E) Macmillan'
-      ],
-      correctAnswer: 'A) A trip to Oregon'
+      'A) A trip to Oregon',
+      'B) A trip to Sahara Desert',
+      'C) A trip to Mexico',
+      'D) A trip to Florida',
+      'E) Macmillan'
+    ],
+    correctAnswer: 'A) A trip to Oregon'
 
-},
-{
+  },
+  {
     question: '3. Who was the main character?',
     answers: [
-        'A) Lady Jessica',
-        'B) Alia',
-        'C) Paul Atreides',
-        'D) Leto Atreides',
-        'E) Leto II Atreides'
-      ],
-      correctAnswer: 'C) Paul Atreides'
+      'A) Lady Jessica',
+      'B) Alia',
+      'C) Paul Atreides',
+      'D) Leto Atreides',
+      'E) Leto II Atreides'
+    ],
+    correctAnswer: 'C) Paul Atreides'
 
-},
-{
+  },
+  {
     question: '4. Who was Paul Atreides’s mother',
     answers: [
-        'A) Lady Jessica',
-        'B) Alia',
-        'C) Chani',
-        'D) Reverend Mother Ramallo',
-        'E) Reverend Mother Mohiam'
-      ],
-      correctAnswer: 'A) Lady Jessica'
+      'A) Lady Jessica',
+      'B) Alia',
+      'C) Chani',
+      'D) Reverend Mother Ramallo',
+      'E) Reverend Mother Mohiam'
+    ],
+    correctAnswer: 'A) Lady Jessica'
 
-},
-{
+  },
+  {
     question: '5. The name of Paul Atreides’s wife:',
     answers: [
-        'A) Margot Fenring',
-        'B) Princess Mohiam',
-        'C) Chani',
-        'D) Princess Irulan',
-        'E) Reverend Mother Ramallo '
-      ],
-      correctAnswer: 'D) Princess Irulan'
+      'A) Margot Fenring',
+      'B) Princess Mohiam',
+      'C) Chani',
+      'D) Princess Irulan',
+      'E) Reverend Mother Ramallo '
+    ],
+    correctAnswer: 'D) Princess Irulan'
 
-},
-{
+  },
+  {
     question: ' 6. The Bene Gesserit litany against fear',
     answers: [
-        'A) Fear is temporary...I will see it go',
-        'B) Nothing in life is to be feared, it is only to be understood',
-        'C) Without darknes snothing comes to birth, as without light nothing flowers',
-        'D) Do not be afraid; our fate cannot be taken from us',
-        'E) I must not fear. For fear is the mind-killer...I will face my fear..'
-      ],
-      correctAnswer: 'E) I must not fear. For fear is the mind-killer...I will face my fear..'
+      'A) Fear is temporary...I will see it go',
+      'B) Nothing in life is to be feared, it is only to be understood',
+      'C) Without darknes snothing comes to birth, as without light nothing flowers',
+      'D) Do not be afraid; our fate cannot be taken from us',
+      'E) I must not fear. For fear is the mind-killer...I will face my fear..'
+    ],
+    correctAnswer: 'E) I must not fear. For fear is the mind-killer...I will face my fear..'
 
-},
-{
+  },
+  {
     question: '7. What are the Fremen’s ultimate goal on Dune?',
     answers: [
-        'A) To rid of their enemy the Harkonnens',
-        'B) To rule Dune',
-        'C) To be recognize as a free people',
-        'D) To create a green paradise in Dune',
-        'E) To escape Dune'
-      ],
-      correctAnswer: 'D) To create a green paradise in Dune'
+      'A) To rid of their enemy the Harkonnens',
+      'B) To rule Dune',
+      'C) To be recognize as a free people',
+      'D) To create a green paradise in Dune',
+      'E) To escape Dune'
+    ],
+    correctAnswer: 'D) To create a green paradise in Dune'
 
-},
-{
+  },
+  {
     question: ' 8. The Padishah Emperor Shaddam IV’s prison planet Salusa Secundus is a front for what?',
     answers: [
-        'A) A training ground for Sardaukar',
-        'B) A secret bunker',
-        'C) A storage space for the spice',
-        'D) A world of paradise',
-        'E) Another Dune'
-        
-      ],
-      correctAnswer: 'A) A training ground for Sardaukar'
+      'A) A training ground for Sardaukar',
+      'B) A secret bunker',
+      'C) A storage space for the spice',
+      'D) A world of paradise',
+      'E) Another Dune'
 
-},
-{
+    ],
+    correctAnswer: 'A) A training ground for Sardaukar'
+
+  },
+  {
     question: '9. Why are there no futuristic robots in the book Dune?',
     answers: [
-        'A) Due to the war with artificial machines, all computers had inspired distrust.',
-        'B) Religion forbids them',
-        'C) A virus wiped all artificial life',
-        'D) People became much more evolved so there was no need for them',
-        'E) They had no recent window update'
-      ],
-      correctAnswer: 'A) Due to the war with artificial machines, all computers had inspired distrust.'
+      'A) Due to the war with artificial machines, all computers had inspired distrust.',
+      'B) Religion forbids them',
+      'C) A virus wiped all artificial life',
+      'D) People became much more evolved so there was no need for them',
+      'E) They had no recent window update'
+    ],
+    correctAnswer: 'A) Due to the war with artificial machines, all computers had inspired distrust.'
 
-},
-{
+  },
+  {
     question: '10. What is a crysknife made out of?',
     answers: [
-        'A) Volcanic rock',
-        'B) Human Bones',
-        'C) Pearls',
-        'D) Gold',
-        'E) Tooth of a sandworm'
-      ],
-      correctAnswer: 'E) Tooth of a sandworm'
+      'A) Volcanic rock',
+      'B) Human Bones',
+      'C) Pearls',
+      'D) Gold',
+      'E) Tooth of a sandworm'
+    ],
+    correctAnswer: 'E) Tooth of a sandworm'
 
-}
+  }
 
 ];
-
-
-
-
-
 
 let score = 0;
 let questionNumber = 0;
 
 
-function updateScore(){
-    score++;
-    $('.score').text(score);
-  }
-  function updateQuestionNumber() {
-    questionNumber++;
-    $('.questionNumber').text(questionNumber + 1);
-  }
+function updateScore() {
+  score++;
+  $('.score').text(score);
+}
+
+function updateQuestionNumber() {
+  questionNumber++;
+  $('.questionNumber').text(questionNumber + 1);
+}
 
 
-function startQuiz(){
+
+
+
+function startQuiz() {
+  $('.altBox').hide();
+  $('.startQuiz').on('click', '.startBtn', function (event) {
+    $('.startQuiz').hide();
+    $('.questionNumber').text(1);
+    $('.questionBox').show();
+    $('.questionBox').prepend(startQuestion());
+  });
+}
+
+
+
+function renderQuestion() {
+  $('.questionBox').html(`
+  <form>
+   <fieldset>
+     <legend class="questionText">${renderStoreQuestion()}</legend>
+      <label class="sizeMe" for="${renderStoreAnswers()[0]}">
+      <input class="js-radio" type="radio" id="${renderStoreAnswers()[0]}" value="${renderStoreAnswers()[0]}" name="answer" required>
+       <span>${renderStoreAnswers()[0]}</span>
+      </label>
+      <label class="sizeMe" for="${renderStoreAnswers()[1]}">
+      <input class="js-radio" type="radio" id="${renderStoreAnswers()[1]}" value="${renderStoreAnswers()[1]}" name="answer" required>
+       <span>${renderStoreAnswers()[1]}</span>
+     </label>
+     <label class="sizeMe" for="${renderStoreAnswers()[2]}">
+     <input class="js-radio" type="radio" id="${renderStoreAnswers()[2]}" value="${renderStoreAnswers()[2]}" name="answer" required>
+      <span>${renderStoreAnswers()[2]}</span>
+    </label>
+    <label class="sizeMe" for="${renderStoreAnswers()[3]}">
+    <input class="js-radio" type="radio" id="${renderStoreAnswers()[3]}" value="${renderStoreAnswers()[3]}" name="answer" required>
+     <span>${renderStoreAnswers()[3]}</span>
+   </label>
+   <label class="sizeMe" for="${renderStoreAnswers()[4]}">
+   <input class="js-radio" type="radio" id="${renderStoreAnswers()[4]}" value="${renderStoreAnswers()[4]}" name="answer" required>
+     <span>${renderStoreAnswers()[4]}</span>
+     </label>
+      <div class= "buttonContainer"><button type="submit" class="submitButton button"> Submit</button > </div>
+  </fieldset>
+</form>
+  `)
+}
+
+
+
+function startQuestion() {
+  if (questionNumber < STORE.length) {
+    return renderQuestion(questionNumber);
+
+  } else {
+    $('.questionBox').hide();
+    finalScore();
+    $('.questionNumber').text(10);
+  }
+
+}
+
+
+
+function submitAnswer() {
+  $('.duneContainer').on('submit', function (event) {
+    event.preventDefault();
     $('.altBox').hide();
-   $('.startQuiz').on('click', '.startBtn', function(event){
-       $('.startQuiz').hide();
-       $('.questionNumber').text(1);
-       $('.questionBox').show();
-       $('.questionBox').prepend(renderAQuestion()); 
-   });
-}
-
-function createHTML(questionIndex) {
-    let formMaker = $(`<form>
-      <fieldset>
-        <legend class="questionText">${STORE[questionIndex].question}</legend>
-      </fieldset>
-    </form>`)
-  
-    let fieldSelector = $(formMaker).find('fieldset');
-  
-    STORE[questionIndex].answers.forEach(function (answerValue, answerIndex) {
-      $(`<label class="sizeMe" for="${answerIndex}">
-          <input class="js-radio" type="radio" id="${answerIndex}" value="${answerValue}" name="answer" required>
-          <span>${answerValue}</span>
-        </label>
-        `).appendTo(fieldSelector);
-    });
-    $(`<div class= "buttonContainer"><button type="submit" class="submitButton button"> Submit</button > </div>`).appendTo(fieldSelector);
-    return formMaker;
-  }
-  
-
-
-function renderAQuestion(){
-    if(questionNumber < STORE.length){
-        return createHTML(questionNumber);
-    }else{
-        $('.questionBox').hide();
-         finalScore();
-         $('.questionNumber').text(10);    
+    $('.response').show();
+    let selected = $('input:checked');
+    let answer = selected.val();
+    let correct = STORE[questionNumber].correctAnswer;
+    if (answer === correct) {
+      correctAnswer();
+    } else {
+      wrongAnswer();
     }
+  });
 
 }
 
 
-
-function submitAnswer(){
-    $('.duneContainer').on('submit', function (event) {
-        event.preventDefault();
-        $('.altBox').hide();
-        $('.response').show();
-        let selected = $('input:checked');
-        let answer = selected.val();
-        let correct = STORE[questionNumber].correctAnswer;
-        if (answer === correct) {
-          correctAnswer();
-        } else {
-          wrongAnswer();
-        }
-      });
-    
-}
-
-
-function nextQuestion(){
-        $('.duneContainer').on('click', '.nextButton', function (event) {
-          $('.altBox').hide();
-          $('.questionBox').show();
-          updateQuestionNumber();
-          $('.questionBox form').replaceWith(renderAQuestion());
-        });         
+function nextQuestion() {
+  $('.duneContainer').on('click', '.nextButton', function (event) {
+    $('.altBox').hide();
+    $('.questionBox').show();
+    updateQuestionNumber();
+    $('.questionBox form').replaceWith(startQuestion());
+  });
 }
 
 
 
 function restartQuiz() {
-    $('.duneContainer').on('click', '.restartButton', function (event) {
-      event.preventDefault();
-      resetStats();
-      $('.altBox').hide();
-      $('.startQuiz').show();
-    });
-  }
+  $('.duneContainer').on('click', '.restartButton', function (event) {
+    event.preventDefault();
+    resetStats();
+    $('.altBox').hide();
+    $('.startQuiz').show();
+  });
+}
 
-  function resetStats() {
-    score = 0;
-    questionNumber = 0;
-    $('.score').text(0);
-    $('.questionNumber').text(0);
-  }
+function resetStats() {
+  score = 0;
+  questionNumber = 0;
+  $('.score').text(0);
+  $('.questionNumber').text(0);
+}
 
-function correctAnswer(){
-    $('.response').html(
-      `<h3>Your answer is correct!</h3>
+function correctAnswer() {
+  $('.response').html(
+    `<h3>Your answer is correct!</h3>
         <p class="sizeMe correctAnswer">Frank Herbert is proud</p>
         <button type="button" class="nextButton button">Next</button>`
-    );
-    updateScore();
-  }
-  
-function wrongAnswer(){
-    $('.response').html(
-        `<div class="wrongAn"><h3>Wrong!</h3></div>
-          <p class="sizeMe wrongAnswer">Frank Herbert is sad</p>
+  );
+  updateScore();
+}
+
+function wrongAnswer() {
+  $('.response').html(
+    `<div class="wrongAn"><h3>Wrong!</h3></div>
+          <p class="sizeMe wrongAnswer">Frank Herbert wanted:</p>
+          <p class="sizeMe">${STORE[questionNumber].correctAnswer}</p>
           <button type="button" class="nextButton button">Next</button>`
-      );
-    
+  );
+
 }
 
 function finalScore() {
@@ -261,14 +274,14 @@ function finalScore() {
   const gradeB = [
     'Nice!',
     'Frank Herbert still loves you'
-   
+
 
   ];
 
   const gradeC = [
     'Please read Dune Again',
     'Frank Herbert will change your life'
-    
+
   ];
 
   if (score >= 8) {
@@ -287,14 +300,29 @@ function finalScore() {
 }
 
 
+function getQuestionPlusAnswers() {
+  return STORE[questionNumber];
+}
 
-function handleQuizClicks(){
-    startQuiz();
-    renderAQuestion();
-    submitAnswer();
-    nextQuestion();
-    restartQuiz();
-    }
+function renderStoreQuestion() {
+  let questionStore = getQuestionPlusAnswers().question;
+  return questionStore;
+}
+
+function renderStoreAnswers() {
+  let eveAnswers = getQuestionPlusAnswers().answers;
+  return eveAnswers;
+}
+
+
+
+function handleQuizClicks() {
+  startQuiz();
+  startQuestion();
+  submitAnswer();
+  nextQuestion();
+  restartQuiz();
+}
 
 
 $(handleQuizClicks);
